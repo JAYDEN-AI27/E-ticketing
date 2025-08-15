2b403653146479673aa1ae984af55d1e9fa1142a
Zzzs1234 <leezs-wm24@student.tarc.edu.my>
2025-08-15 11:16:02 +0800

    database file cannot generate
    tmr ask teacher

---
+++ Demo/Demo/Migrations/20250815031116_CreateDB.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    TicketID = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UnitPrice = table.Column<decimal>(type: "decimal(4,2)", precision: 4, scale: 2, nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    DepartureTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Destination = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.TicketID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Hash = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(8)", maxLength: 8, nullable: false),
+                    PhotoURL = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", maxLength: 4, nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(5,2)", precision: 5, scale: 2, nullable: false),
+                    UserEmail = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_UserEmail",
+                        column: x => x.UserEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderTicket",
+                columns: table => new
+                {
+                    OrdersOrderID = table.Column<int>(type: "int", nullable: false),
+                    TicketsTicketID = table.Column<string>(type: "nvarchar(4)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderTicket", x => new { x.OrdersOrderID, x.TicketsTicketID });
+                    table.ForeignKey(
+                        name: "FK_OrderTicket_Orders_OrdersOrderID",
+                        column: x => x.OrdersOrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderTicket_Tickets_TicketsTicketID",
+                        column: x => x.TicketsTicketID,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserEmail",
+                table: "Orders",
+                column: "UserEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderTicket_TicketsTicketID",
+                table: "OrderTicket",
+                column: "TicketsTicketID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderTicket");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- Demo/Demo/Models/DB.cs
+++ Demo/Demo/Models/DB.cs
@@ -13,6 +13,8 @@ public class DB : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Admin> Admins { get; set; }
     public DbSet<Member> Members { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Ticket> Tickets { get; set; }
 }
 
 // Entity Classes -------------------------------------------------------------
@@ -28,6 +30,7 @@ public class User
 
     public string Role => GetType().Name;
     // TODO
+    public List<Order> Orders { get; set; } = [];
 }
 
 // TODO
@@ -41,4 +44,34 @@ public class Member: User
 {
     [MaxLength(100)]
     public string PhotoURL { get; set; }
+}
+
+public class Order
+{
+    [Key,MaxLength(4)]
+    public int OrderID { get; set; }
+    public DateTime OrderDate { get; set; }
+    [Precision(5,2)]
+    public decimal Amount { get; set; }
+
+    public User User { get; set; }
+    public List<Ticket> Tickets { get; set; } = [];
+}
+
+public class Ticket
+{
+    [Key,MaxLength(4)]
+    public string TicketID { get; set; }
+    [MaxLength(100)]
+    public string Type { get; set; }
+    [Precision(4,2)]
+    public decimal UnitPrice { get; set; }
+    public int Stock {  get; set; }
+    public DateTime DepartureTime {  get; set; }
+    [MaxLength(100)]
+    public string Source {  get; set; }
+    [MaxLength(100)]
+    public string Destination { get; set; }
+
+    public List<Order> Orders { get; set; } = [];
 }
