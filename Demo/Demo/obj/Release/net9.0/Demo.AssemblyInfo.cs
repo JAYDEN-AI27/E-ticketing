06c6d044d91ea32fdf5c3be90a2212d03f4560e6
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-21 12:20:34 +0800

    dde
    fd

--- Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b403653146479673aa1ae984af55d1e9fa1142a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3822dbb8a4c4f34b6cc6763e6e001d70ada31b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
