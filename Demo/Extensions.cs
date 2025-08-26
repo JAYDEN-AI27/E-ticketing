5cb9784e7fb8090569f4e268b08e423e06c38ab3
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-26 10:05:21 +0800

    mm

---
+++ Demo/Extensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Demo;
+
+public static class Extensions
+{
+    public static bool IsAjax(this HttpRequest request)
+    {
+        return request.Headers.XRequestedWith == "XMLHttpRequest";
+    }
+
+    public static bool IsValid(this ModelStateDictionary ms, string key)
+    {
+        return ms.GetFieldValidationState(key) == ModelValidationState.Valid;
+    }
+
+    public static DateOnly ToDateOnly(this DateTime dt)
+    {
+        return DateOnly.FromDateTime(dt);
+    }
+
+    public static TimeOnly ToTimeOnly(this DateTime dt)
+    {
+        return TimeOnly.FromDateTime(dt);
+    }
+}
