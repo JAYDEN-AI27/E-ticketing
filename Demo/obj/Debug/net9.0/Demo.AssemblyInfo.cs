f65c4c46b0cdbe8ad360437fbaa9991d757d1a3b
Zzzs1234 <leezs-wm24@student.tarc.edu.my>
2025-08-30 16:20:32 +0800

    delete demo2 3 4

--- Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+014271519c80b7fe6eef06b94cca1e3f17139b80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe816e70cef498ab199c7ed78e6ed199eb1bd46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
