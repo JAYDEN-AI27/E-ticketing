ed2775e2925d97cf25d9e669f5556998939671bf
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-30 17:32:19 +0800

    new

---
+++ Demo/Demo/Models/DB.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models;
+
+#nullable disable warnings
+
+public class DB : DbContext
+{
+    public DB(DbContextOptions options) : base(options) { }
+
+    // DB Sets
+    public DbSet<User> Users { get; set; }
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Member> Members { get; set; }
+    public DbSet<Event> Events { get; set; }
+}
+
+// Entity Classes -------------------------------------------------------------
+
+public class User
+{
+    [Key, MaxLength(100)]
+    public string Email { get; set; }
+    [MaxLength(100)]
+    public string Hash { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    public string Role => GetType().Name;
+}
+
+public class Admin : User
+{
+
+}
+
+public class Member : User
+{
+    [MaxLength(100)]
+    public string PhotoURL { get; set; }
+}
+
+public class Event
+{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly Time { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; }
+}
---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,23 @@
+global using Demo.Models;
+global using Demo;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddSqlServer<DB>($@"
+    Data Source=(LocalDB)\MSSQLLocalDB;
+    AttachDbFilename={builder.Environment.ContentRootPath}\DB.mdf;
+");
+builder.Services.AddScoped<Helper>();
+builder.Services.AddAuthentication().AddCookie();
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+// Culture = en-MY, ms-MY, zh-CN, ja-JP, ko-KR, etc.
+// TODO
+app.UseRequestLocalization("en-MY");
+
+app.MapDefaultControllerRoute();
+app.Run();
