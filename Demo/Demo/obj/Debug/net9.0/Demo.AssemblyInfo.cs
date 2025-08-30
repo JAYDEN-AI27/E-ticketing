236782a00f5fbb3d5f29b1943300792d3391169c
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-30 18:35:32 +0800

    databse build failed

--- Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2775e2925d97cf25d9e669f5556998939671bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e06eeeeee6b4e344ce529de6eea751db5c33aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
