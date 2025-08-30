ce30d6676e0ec1d6b1d58d70e1e485d365746799
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-30 18:45:42 +0800

    failed again

--- Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37e06eeeeee6b4e344ce529de6eea751db5c33aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+236782a00f5fbb3d5f29b1943300792d3391169c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
