ebef7b64b1c3de7f658abdca31bf17aff31fcf7e
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-08-30 16:38:16 +0800

    mm

--- Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -15,7 +15,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fe816e70cef498ab199c7ed78e6ed199eb1bd46")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+014271519c80b7fe6eef06b94cca1e3f17139b80")]
+>>>>>>> Stashed changes
 =======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+014271519c80b7fe6eef06b94cca1e3f17139b80")]
 >>>>>>> Stashed changes
