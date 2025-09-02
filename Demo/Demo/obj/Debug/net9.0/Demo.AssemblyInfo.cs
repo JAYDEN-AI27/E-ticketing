8ac4030cfd0258f1875b2b093bd8118e56d8d347
Chong1211 <chongjw-wm24@student.tarc.edu.my>
2025-09-02 22:59:47 +0800

    Ticket

--- Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d911f88e2c4a699f693f899e445d24196c75b22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85d64e0c072a3e4abf8763b1ad16f59adcd0049")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
