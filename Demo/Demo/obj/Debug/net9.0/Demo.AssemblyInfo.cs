138d45f9a8e92f590ab9e7413f7c5fb989081177
JAYDEN-AI27 <lizhecha76@gmail.com>
2025-09-16 15:02:01 +0800

    .

--- Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb973da18b80d2cebbc8e4b47687d05c7b11240d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b8b93f70259622b094497b560f11a15f6abb77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Release/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d76df138c6f024e958f9a66899ff91eaa215d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b8b93f70259622b094497b560f11a15f6abb77")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
