86250c4204c4a2c6e8ec5585c26abb3dd7be5147
Ren-0509 <tanjr-wm24@student.tarc.edu.my>
2025-09-19 00:25:28 +0800

    yameidie

--- Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
+++ Demo/Demo/obj/Debug/net9.0/Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a679229b7f80586dd923f4c980e812eb3789dd2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b0eba387e7e3a44e5a9b8a316d90e93af404b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
